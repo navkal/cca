f1d91bdcf508205190217b45584545cf1cd16f77
navkal <navkal@hotmail.com>
2019-04-28 15:23:39 -0400

    Initial

---
+++ about/about.php
@@ -0,0 +1,15 @@
+<?php
+  // Copyright 2019 Andover CCA.  All rights reserved.
+?>
+
+<div class="container">
+
+  <p class="h5">
+    Community Choice Aggregation (CCA)
+  </p>
+
+  <p>
+    This is a stub.
+  </p>
+
+</div>
---
+++ faq/faq.php
@@ -0,0 +1,19 @@
+<?php
+  // Copyright 2019 Andover CCA.  All rights reserved.
+?>
+
+<div class="container">
+
+  <p class="h5">
+    Community Choice Aggregation (CCA)
+  </p>
+
+  <p>
+    <b>Q</b>: Duh?
+  </p>
+
+  <p>
+    <b>A</b>: Yuh!
+  </p>
+
+</div>
