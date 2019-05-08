f056510529970358f7f4c8ae0b650e0e12417877
navkal <navkal@hotmail.com>
2019-05-08 13:29:31 -0400

    Calculation

--- learnmore/learnmore.php
+++ learnmore/learnmore.php
@@ -1,5 +1,9 @@
 <?php
   // Copyright 2019 Andover CCA.  All rights reserved.
+
+  // Description list layout
+  $sDtClass = 'dl-title col-sm-5 col-md-4 col-lg-3';
+  $sDdClass = 'col-sm-7 col-md-8 col-lg-9';
 ?>
 
 <div class="container">
@@ -8,8 +12,38 @@
     Learn more about <i>Community Choice Aggregation</i>
   </p>
 
-  <p>
-    This is a stub.
+  <p class="h6 row mt-4">
+    CCA Program Details
   </p>
+
+  <dl class="row">
+
+    <dt class="<?=$sDtClass?>"><a href="https://www.mass.gov/info-details/municipal-aggregation" target="_blank">Mass.gov - CCA</a></dt>
+    <dd class="<?=$sDdClass?>">CCA (a.k.a Municipal Aggregation) explained</dd>
+
+    <dt class="<?=$sDtClass?>"><a href="https://www.mapc.org/resource-library/start-a-community-choice-aggregation-program/" target="_blank">Metropolitan Area Planning Council</a></dt>
+    <dd class="<?=$sDdClass?>">Promoting Smart Growth & Regional Planning</dd>
+
+    <dt class="<?=$sDtClass?>"><a href="https://drive.google.com/open?id=1D7E_xN45nHt2MoVBcy5JylLRv0Ek5q7B" target="_blank">Merrimack Valley Map</a></dt>
+    <dd class="<?=$sDdClass?>">Nearby towns offering CCA</dd>
+
+    <dt class="<?=$sDtClass?>"><a href="https://docs.google.com/spreadsheets/d/1wNX4zTMuKPAL0QSGVU3hSx6O6sE8keQXy8Zwx2Tbh0c/edit?usp=sharing" target="_blank">Current contracts</a></dt>
+    <dd class="<?=$sDdClass?>">CCA pricing in nearby towns</dd>
+
+  </dl>
+
+  <p class="h6 row mt-4">
+    Related Topics
+  </p>
+
+  <dl class="row">
+
+    <dt class="<?=$sDtClass?>"><a href="http://energyswitchma.gov/" target="_blank">Energy Switch</a></dt>
+    <dd class="<?=$sDdClass?>">Choosing an electrical energy source</dd>
+
+    <dt class="<?=$sDtClass?>"><a href="https://commonwealthmagazine.org/opinion/municipal-electricity-aggregation-really-works/" target="_blank">Commonwealth Magazine</a></dt>
+    <dd class="<?=$sDdClass?>">Success story in Newton</dd>
+
+  </dl>
 
 </div>
