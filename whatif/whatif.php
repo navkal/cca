0a4ae58241a8d65b915742b82e340bc08c008d7c
navkal <navkal@hotmail.com>
2019-05-10 12:27:41 -0400

    Calculation

--- whatif/whatif.php
+++ whatif/whatif.php
@@ -183,7 +183,7 @@ input.error
 
 <!-- Help modal dialog -->
 <div class="modal fade" id="help-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-lg" role="document">
+  <div class="modal-dialog modal-md" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">CCA Bill Estimator</h5>
@@ -204,7 +204,7 @@ input.error
             </b>
             <ul>
               <li>
-                <i>Standard</i> - Meets or exceeds renewable energy requirements established by MA
+                <i>Standard</i> - Meets or exceeds MA renewable energy requirements
               </li>
               <li>
                 <i>Green</i> - 100% renewable, typically generated outside of MA
