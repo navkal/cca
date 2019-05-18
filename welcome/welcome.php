4da21ec88980410ee390d083bc87ecc21cce1323
navkal <navkal@hotmail.com>
2019-05-18 16:28:42 -0400

    Calculation

--- welcome/welcome.php
+++ welcome/welcome.php
@@ -4,13 +4,13 @@
 
 <div class="jumbotron jumbotron-fluid">
   <div class="container">
-    <h1 class="display-4">Hello, Andover!</h1>
+    <h1 class="display-4">Welcome!</h1>
 
     <div class="text-center" >
       <img src="welcome/banner.jpg" class="img-fluid" alt="Andover CCA">
     </div>
     <br/>
-    <p class="lead">Tell your Select Board to bring CCA to Andover.</p>
+    <p class="lead">Let's bring CCA to Andover.</p>
   </div>
 </div>
 
