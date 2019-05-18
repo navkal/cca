82e8bf39d92db6d68d096d0453243de328749f93
navkal <navkal@hotmail.com>
2019-05-18 16:51:41 -0400

    Calculation

--- welcome/welcome.php
+++ welcome/welcome.php
@@ -1,14 +1,27 @@
 <?php
   // Copyright 2019 Andover CCA.  All rights reserved.
 ?>
+<style>
+  body
+  {
+    background-image: url( "welcome/banner.jpg" );
+    background-position: center top;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: cover;
+  }
+
+  .jumbotron
+  {
+    background-color: transparent;
+    color: white;
+  }
+
+</style>
 
 <div class="jumbotron jumbotron-fluid">
   <div class="container">
     <h1 class="display-4">Welcome!</h1>
-
-    <div class="text-center" >
-      <img src="welcome/banner.jpg" class="img-fluid" alt="Andover CCA">
-    </div>
     <br/>
     <p class="lead">Let's bring CCA to Andover.</p>
   </div>
