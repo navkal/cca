99800bae9886c14ccdc7751ff4d61ff22cab7390
navkal <navkal@hotmail.com>
2019-05-20 19:52:15 -0400

    Calculation

---
+++ basicservice/basicservice.php
@@ -0,0 +1,45 @@
+<?php
+  // Copyright 2019 Andover CCA.  All rights reserved.
+?>
+
+<div class="text-center">
+  <img src="basicservice/basicservice.png" class="img-fluid" style="margin:auto; width:100%; max-width:900px" alt="Basic Service">
+</div>
+
+<br/>
+
+<div class="container">
+
+  <p class="h5 mt-1">
+    What is Basic Service?
+  </p>
+
+  <p>
+    Your electric service has two components:
+    <ul class="list-unstyled" >
+      <li>
+        <b>Supply</b> - Generation and procurement of the electricity you use
+      </li>
+      <li>
+        <b>Delivery</b> - Transmission and distribution of the electricity to your home or business
+      </li>
+    </ul>
+  </p>
+
+  <p>
+    The electricity <i>supplied</i> by National Grid is known as <b><i>Basic Service</i></b>.
+  </p>
+
+  <p>
+    In the past, only your electric utility could provide your electricity supply.
+    But now, under MA state law, consumers can choose a supplier other than their utility.
+    In this case, the utility would continue to deliver the electricity, maintain poles and wires, and provide other customer services; and customers would continue to receive a single electric bill from their utility.
+  </p>
+
+  <p>
+    Today, most residents and businesses subscribe to their utility's Basic Service.
+    <a href="/?page=overview" >Community Choice Aggregation</a> is a state program that permits a town to choose an electricity supplier on behalf of its residents and businesses.
+    Consumers who subscribe to Basic Service are automatically enrolled with the ability to opt out at any time with no penalty.
+  </p>
+
+</div>
--- disclaimer.php
+++ disclaimer.php
@@ -9,7 +9,7 @@
       Disclaimer
     </h6>
     <i>
-      A goal of the aggregation program is to produce savings for customers, but savings cannot be guaranteed compared to the utility’s basic service rate, which changes every six months for Residential and Small Business customers and every three months for Large Business customers.
+      A goal of the aggregation program is to produce savings for customers, but savings cannot be guaranteed compared to the utility’s Basic Service rate, which changes every six months for Residential and Small Business customers and every three months for Large Business customers.
       The aggregation program seeks to provide price stability and average savings over the full term of the program, but because future basic service rates are not known, there is no guarantee of savings.
     </i>
   </small>
