aa285ffc16caaf7524b7473f396f82e39eae5b97
navkal <navkal@hotmail.com>
2019-05-27 15:19:14 -0400

    FAQ

--- faq/faq.php
+++ faq/faq.php
@@ -1,5 +1,42 @@
 <?php
   // Copyright 2019 Andover CCA.  All rights reserved.
+
+
+  $aFaq =
+  [
+    [
+      'q' => 'Whahhh?',
+      'a' => 'Duhhhh!'
+    ],
+    [
+      'q' => 'Whahhh?',
+      'a' => 'Duhhhh!'
+    ],
+    [
+      'q' => 'Whahhh?',
+      'a' => 'Duhhhh!'
+    ],
+    [
+      'q' => 'Whahhh?',
+      'a' => 'Duhhhh!'
+    ],
+    [
+      'q' => 'Whahhh?',
+      'a' => 'Duhhhh!'
+    ],
+    [
+      'q' => 'Whahhh?',
+      'a' => 'Duhhhh!'
+    ],
+    [
+      'q' => 'Whahhh?',
+      'a' => 'Duhhhh!'
+    ],
+    [
+      'q' => 'Whahhh?',
+      'a' => 'Duhhhh!'
+    ],
+  ];
 ?>
 
 <style>
@@ -11,34 +48,26 @@
     background-attachment: fixed;
     background-size: cover;
   }
-
-  .jumbotron
-  {
-    background-color: transparent;
-    color: #384897;
-  }
-
-  .cca-link
-  {
-    color: #384897;
-  }
-
 </style>
 
 <div class="container">
+
   <div class="row">
     <div class="col-10 mx-auto">
-      <div class="accordion" id="faqExample">
+      <div class="h5 py-1">
+        Frequently Asked Questions
+      </div>
+      <div class="accordion" id="faq">
 
         <div class="card">
-          <div class="card-header p-2" id="headingOne">
+          <div class="card-header p-2" id="q1">
             <h5 class="mb-0">
-              <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+              <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#a1" aria-expanded="true" aria-controls="a1">
                 Q: How does this work?
               </button>
             </h5>
           </div>
-          <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#faqExample">
+          <div id="a1" class="collapse" aria-labelledby="q1" data-parent="#faq">
             <div class="card-body">
               <b>Answer:</b> It works using the Bootstrap 4 collapse component with cards to make a vertical accordion that expands and collapses as questions are toggled.
             </div>
@@ -46,14 +75,14 @@
         </div>
 
         <div class="card">
-          <div class="card-header p-2" id="headingTwo">
+          <div class="card-header p-2" id="q2">
             <h5 class="mb-0">
-            <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+            <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#a2" aria-expanded="false" aria-controls="a2">
               Q: What is Bootstrap 4?
             </button>
             </h5>
           </div>
-          <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#faqExample">
+          <div id="a2" class="collapse" aria-labelledby="q2" data-parent="#faq">
             <div class="card-body">
                 Bootstrap is the most popular CSS framework in the world. The latest version released in 2018 is Bootstrap 4. Bootstrap can be used to quickly build responsive websites.
             </div>
@@ -61,14 +90,14 @@
         </div>
 
         <div class="card">
-          <div class="card-header p-2" id="headingThree">
+          <div class="card-header p-2" id="q3">
             <h5 class="mb-0">
-              <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+              <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#a3" aria-expanded="false" aria-controls="a3">
                 Q. What is another question?
               </button>
             </h5>
           </div>
-          <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#faqExample">
+          <div id="a3" class="collapse" aria-labelledby="q3" data-parent="#faq">
             <div class="card-body">
               The answer to the question can go here.
             </div>
@@ -76,14 +105,14 @@
         </div>
 
         <div class="card">
-          <div class="card-header p-2" id="headingThree">
+          <div class="card-header p-2" id="q4">
             <h5 class="mb-0">
-              <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+              <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#a4" aria-expanded="false" aria-controls="a4">
                 Q. What is the next question?
               </button>
             </h5>
           </div>
-          <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#faqExample">
+          <div id="a4" class="collapse" aria-labelledby="q4" data-parent="#faq">
             <div class="card-body">
               The answer to this question can go here. This FAQ example can contain all the Q/A that is needed.
             </div>
